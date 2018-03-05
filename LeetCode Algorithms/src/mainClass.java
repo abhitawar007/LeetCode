67e3e8abab0fe7cfa83f0000e7c3ee624f8dfefc
Abhijeet <abhijeet.tawar007@gmail.com>
2018-03-04 20:52:47 -0500

    Reverse a linked list

--- LeetCode Algorithms/src/mainClass.java
+++ LeetCode Algorithms/src/mainClass.java
@@ -1,28 +1,23 @@
-import java.util.HashMap;
-
-import javax.management.ListenerNotFoundException;
-
-import algorithms.FirstBadVersion;
 import algorithms.LinkedListPalindrome;
 import algorithms.ListNode;
-import algorithms.TaskScheduler;
+import algorithms.ReverseLinkedList;
 
-public class mainClass {
+public class mainClass
+{
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		// Task Scheduler
-//		char[] arr = {'A','A','A','B','B','B'};
-//		char[] arr = {'A','A','A','A','A','A'};
+		// char[] arr = {'A','A','A','B','B','B'};
+		// char[] arr = {'A','A','A','A','A','A'};
 
-//		TaskScheduler t = new TaskScheduler();
-//		System.out.println(t.leastInterval(arr, 2));
-		
-		
+		// TaskScheduler t = new TaskScheduler();
+		// System.out.println(t.leastInterval(arr, 2));
+
 		// FirstBadVersion
-//		FirstBadVersion f = new FirstBadVersion();
-//		System.out.println(f.firstBadVersion(10));
-		
+		// FirstBadVersion f = new FirstBadVersion();
+		// System.out.println(f.firstBadVersion(10));
+
 		// Linked list palindrome
 		LinkedListPalindrome list = new LinkedListPalindrome();
 		// Generate Linked list
@@ -38,28 +33,29 @@ public class mainClass {
 		c1 = c1.next;
 		c1.next = new ListNode(6);
 		c1 = c1.next;
-		
-		
-		ListNode h2 = new ListNode(1);
+
+		ListNode h2 = new ListNode(3);
 		ListNode c2 = h2;
-		c2.next = new ListNode(2);
+		c2.next = new ListNode(5);
 		c2 = c2.next;
-//		c2.next = new ListNode(2);
-//		c2 = c2.next;
-//		c2.next = new ListNode(1);
-//		c2 = c2.next;
-		
-		list.reverseList(h1);
-		System.out.println("========================");
-		list.reverseList(h2);
-//		System.out.println(list.isPalindrome(h1));
-//		System.out.println(list.isPalindrome(h2));
-		
-		
-//		HashMap<Character, Integer> map = new HashMap<>();
-//		map.put('a', map.getOrDefault('a', 1) + 1);
-//		System.out.println(map);
-		
+		// c2.next = new ListNode(2);
+		// c2 = c2.next;
+		// c2.next = new ListNode(1);
+		// c2 = c2.next;
+
+		// list.reverseList(h1);
+		// System.out.println("========================");
+		// list.reverseList(h2);
+		// System.out.println(list.isPalindrome(h1));
+		// System.out.println(list.isPalindrome(h2));
+
+		ReverseLinkedList rvlist = new ReverseLinkedList();
+		rvlist.reverseMtoN(h1, 1, 4);
+
+		// HashMap<Character, Integer> map = new HashMap<>();
+		// map.put('a', map.getOrDefault('a', 1) + 1);
+		// System.out.println(map);
+
 	}
 
 }
