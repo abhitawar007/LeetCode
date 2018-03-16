2916c957c44de81f7b7b4101c548d003170b54bc
Abhijeet <abhijeet.tawar007@gmail.com>
2018-03-16 16:42:21 -0400

    More string algorithms

--- LeetCode Algorithms/src/testing/ArrayAndStringTesting.java
+++ LeetCode Algorithms/src/testing/ArrayAndStringTesting.java
@@ -9,8 +9,11 @@ public class ArrayAndStringTesting
 	{
 		ArraysAndStrings arrayNstr = new ArraysAndStrings();
 
-		String[] strarray = { "eat", "tea", "tan", "ate", "nat", "bat" };
-		System.out.println(arrayNstr.GroupAnagrams(strarray));
+		// String[] strarray = { "eat", "tea", "tan", "ate", "nat", "bat" };
+		// System.out.println(arrayNstr.GroupAnagrams(strarray));
+
+		System.out.println("Longest substring length w/o repeatation : "
+				+ arrayNstr.lengthOfLongestSubstring("abcabcbb"));
 	}
 
 }
