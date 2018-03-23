9571f16cd50672c32723844ee7b9cd58a3446d3c
Abhijeet <abhijeet.tawar007@gmail.com>
2018-03-23 16:41:55 -0400

    Added Integer algos

--- LeetCode Algorithms/src/mainClass.java
+++ LeetCode Algorithms/src/mainClass.java
@@ -3,16 +3,6 @@ public class mainClass
 
 	public static void main(String[] args)
 	{
-		// Task Scheduler
-		// char[] arr = {'A','A','A','B','B','B'};
-		// char[] arr = {'A','A','A','A','A','A'};
-
-		// TaskScheduler t = new TaskScheduler();
-		// System.out.println(t.leastInterval(arr, 2));
-
-		// FirstBadVersion
-		// FirstBadVersion f = new FirstBadVersion();
-		// System.out.println(f.firstBadVersion(10));
 
 	}
 
---
+++ LeetCode Algorithms/src/testing/IntegerTesring.java
@@ -0,0 +1,17 @@
+package testing;
+
+import algorithms.IntegerAlgos;
+
+public class IntegerTesring
+{
+	public static void main(String[] args)
+	{
+		IntegerAlgos in = new IntegerAlgos();
+
+		// __________________________________________________________
+
+		// Multiply w/o *
+		System.out.println("Multiplication : " + in.multiply(7, 9));
+		// __________________________________________________________
+	}
+}
