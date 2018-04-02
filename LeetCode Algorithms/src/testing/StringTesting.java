56532b36efb4d18bdead29a516b57cce7e6e9a12
Abhijeet <abhijeet.tawar007@gmail.com>
2018-04-01 20:51:35 -0400

    String algo and array algo seperated

---
+++ LeetCode Algorithms/src/testing/StringTesting.java
@@ -0,0 +1,28 @@
+package testing;
+
+import algorithms.StringAlgos;
+
+public class StringTesting
+{
+
+	public static void main(String[] args)
+	{
+		StringAlgos st = new StringAlgos();
+
+		// String[] strarray = { "eat", "tea", "tan", "ate", "nat", "bat" };
+		// System.out.println(arrayNstr.GroupAnagrams(strarray));
+
+		// System.out.println("Longest substring length w/o repeatation : "
+		// + arrayNstr.lengthOfLongestSubstring("abcabcbb"));
+
+		// int[] arr = { 1, 2, 2, 1 };
+		// System.out.println("Increasing triplet : " +
+		// arrayNstr.increasingTriplet(arr));
+		// _______________________________________________________
+		// System.out.println("Generated paranthesis");
+		// System.out.println(arrayNstr.generateParanthesis(3));
+		// _______________________________________________________
+
+	}
+
+}
